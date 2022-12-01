7e45c21fada1aceef731c41244a98ace478bd2c8
AndrejNil <and.kucheroff@yandex.ru>
2022-12-01 21:45:21 +0500

    'edit'

--- database/seeders/DeveloperSeeder.php
+++ database/seeders/DeveloperSeeder.php
@@ -3,16 +3,19 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
-class DeveloperSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        //
-    }
+class DeveloperSeeder extends Seeder {
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run() {
+    DB::table('developers')->insert([
+      'title' => Str::random(10),
+      'description' => Str::random(10),
+    ]);
+  }
 }
